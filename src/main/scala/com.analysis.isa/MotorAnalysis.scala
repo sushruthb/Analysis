babad89e184e05388aced32d5a13a06374769410
hdfsuser <hdfs@localhost.localdomain>
2018-08-09 10:03:54 +0530

    modified motorAnalysis.scala

--- src/main/scala/com.analysis.isa/MotorAnalysis.scala
+++ src/main/scala/com.analysis.isa/MotorAnalysis.scala
@@ -1,5 +1,5 @@
 import org.apache.spark._
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql._
 import org.apache.log4j._
 object MotorAnalysis{
 
@@ -11,11 +11,15 @@ object MotorAnalysis{
 
     val data=spark.read.option("header","true").option("inferSchema","true").csv("hdfs://localhost:9000/user/hdfs/motordata.csv")
 
-    data.createTempView("test")
+  //  data.createTempView("test")
 
-    spark.sql("select motor_id from test where speed ==(select max(speed) from test) ").show()
+ //   spark.sql("select motor_id from test where speed ==(select max(speed) from test) ").show()
+   //spark.sql("select max(speed) from test").show
 
-    //spark.sql("select max(speed) from test").show
-
+val table=data.createOrReplaceTempView("TempView")
+    val maxSpeed= spark.sql("select motor_id,speed  from TempView where speed = (select max(speed) from TempView)").show()
+    val maxTorque=spark.sql("select motor_id,torque from TempView where torque=(select max(torque) from TempView)").show()
+    val maxPowerinwatt =spark.sql("select motor_id,powerinwatt from TempView where powerinwatt =(select max(powerinwatt) from TempView)").show()
+  
   }
 }
